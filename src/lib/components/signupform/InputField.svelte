d2f34f3a05988fcfe9e0228de4d68040ad8d92ae
ajvarus <arjunsen7@protonmail.com>
2024-06-12 13:11:04 +0100

    Changed the Tailwind theme and added some shadcn components

---
+++ src/lib/components/signupform/InputField.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { Button } from "$lib/components/ui/button/index.js";
+  import { Input } from "$lib/components/ui/input/index.js";
+
+  import LoaderCircle from "lucide-svelte/icons/loader-circle";
+
+  let isLoading = $state(false);
+</script>
+
+<form class="flex w-full max-w-sm items-center space-x-2">
+  <Input type="email" placeholder="email" />
+  <Button type="submit" on:click={() => (isLoading = !isLoading)}>
+    {#if isLoading}
+      <LoaderCircle class="mr-2 h-4 w-4 animate-spin" />
+    {/if}
+    Subscribe</Button
+  >
+</form>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import InputField from "$lib/components/signupform/InputField.svelte";
+</script>
+
+<div class="flex justify-center items-center h-screen">
+  <InputField />
+</div>
