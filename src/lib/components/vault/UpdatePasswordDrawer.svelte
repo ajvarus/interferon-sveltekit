e7bee5742611f24a29f78e6bf0a68caf6286b742
Arjun Sen <arjunsen1997@gmail.com>
2024-07-11 23:39:31 +0100

    Addressed some UI bugs on the main vault page. Some animations are still buggy.

--- src/lib/components/vault/UpdatePasswordDrawer.svelte
+++ src/lib/components/vault/UpdatePasswordDrawer.svelte
@@ -22,12 +22,19 @@
   } = $props();
 </script>
 
-<Drawer.Root>
-  <Drawer.Trigger asChild let:builder>
-    <Button builders={[builder]} size="icon" variant="outline"
-      ><Pencil class="w-4 h-4" /></Button
-    >
-  </Drawer.Trigger>
+<Drawer.Root bind:open={upc.drawerOpen} onOpenChange={() => upc.toggleDrawer()}>
+  <Button
+    size="icon"
+    variant="outline"
+    on:click={() => {
+      if (isUnlocked) upc.openDrawer();
+      else
+        toast.error("Password locked", {
+          description: "Unlock the password to edit.",
+          duration: 5000,
+        });
+    }}><Pencil class="w-4 h-4" /></Button
+  >
   <Drawer.Content>
     <div class="mx-auto w-full max-w-sm">
       <Drawer.Header>
@@ -35,9 +42,9 @@
       </Drawer.Header>
       <PasswordUpdateCard {password} />
       <Drawer.Footer>
-        <Drawer.Close asChild let:builder>
-          <Button builders={[builder]} variant="outline">Cancel</Button>
-        </Drawer.Close>
+        <Button variant="outline" on:click={() => upc.closeDrawer()}
+          >Cancel</Button
+        >
       </Drawer.Footer>
     </div>
   </Drawer.Content>
